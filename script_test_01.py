81a0b29a20a56899e79b79513ec7e433e2484ba8
Code Scholar <tembohamilton@gmail.com>
2024-06-25 22:59:50 +0200

    simple introduction to python

---
+++ script_test_01.py
@@ -0,0 +1,7 @@
+#This file is just a test file to prepare for python variables
+#variables are memory locations.
+name = "chisomo"
+print(name)
+# Here the variable is name and inside that variable there is the string chisomo
+age = "17"
+print("Suzen is the same age as "+ name + " and the age is "+ age)
