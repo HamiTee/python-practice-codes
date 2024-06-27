ab1df38c5586f332d02c8e467b90063825c38d7b
Code Scholar <tembohamilton@gmail.com>
2024-06-27 23:32:45 +0200

    python lists intro

---
+++ py_lists.py
@@ -0,0 +1,15 @@
+# A List in Python is a compound daa type which can contain more than one type of
+# data type including the list itself. The following is an example.
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath ]
+
+# Print areas
+print(areas)
